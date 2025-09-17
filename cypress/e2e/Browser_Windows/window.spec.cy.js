3ffd0cee24cf04f2ab45a104f33c8ac3b06e9e15
Daniel Gregini Trovão <damtrovao@gmail.com>
2025-09-17 12:14:49 -0300

    Inclusão do teste de nova janela - Etapa 2

---
+++ cypress/e2e/Browser_Windows/window.spec.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+import { WindowPage } from '../../pages/WindowPage'
+
+// O Cypress, propositalmente, não lida com múltiplas janelas ou abas. Para contornar essa limitação, quebrei o teste em duas etapas:
+// 1. Validar que o botão abre uma nova janela (sem realmente abrir a nova janela).
+// 2. Validar o conteúdo da nova janela, forçando o link a abrir na mesma aba, para que o Cypress consiga "visualizar" o conteúdo da nova página aberta.
+
+describe('New Window', () => {
+    before(() => {
+    })
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Teste 02 - Validar Abrir nova Janela', () => {
+        WindowPage.navegar()
+        WindowPage.clicarNewWindowValidarAbertura()
+        WindowPage.fecharNewWindow()
+    })
+
+    it('Teste 03 - Validar conteúdo da nova Janela', () => {
+        WindowPage.navegar()
+        WindowPage.clicarNewWindowValidarConteúdo()
+        WindowPage.fecharNewWindow()
+    })
+})
---
+++ cypress/pages/WindowPage.js
@@ -0,0 +1,54 @@
+export const WindowPage = {
+  elements: {
+    btnWindow: () => cy.contains('.card-body', 'Alerts, Frame & Windows'),
+    btnBrowserWindows: () => cy.contains('.element-list .menu-list li', 'Browser Windows'),
+    btnNewWindow: () => cy.get('#windowButton'),
+    textNewWindow: () => cy.get('#sampleHeading'),
+  },
+
+  navegar() {
+    this.elements.btnWindow().click()
+    this.elements.btnBrowserWindows().click()
+  },
+
+  clicarNewWindowValidarAbertura() {
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as('windowOpen')
+    })
+
+    this.elements.btnNewWindow().click()
+
+    cy.get('@windowOpen').should('be.called')
+  },
+
+  clicarNewWindowValidarConteúdo() {
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as("newWindow");
+    })
+
+    this.elements.btnNewWindow().click();
+
+    cy.get('@newWindow').should('have.been.called');
+    cy.get('@newWindow').then((stub) => {
+      const firstCall = stub.getCall(0);
+      const urlArg = firstCall && firstCall.args && firstCall.args[0];
+
+      const targetUrl = (urlArg && urlArg !=='about:blank') ? urlArg : 'https://demoqa.com/sample';
+
+      if (targetUrl) {
+        cy.visit(targetUrl);
+      }
+    });
+
+    this.elements.textNewWindow().should('have.text', 'This is a sample page')
+  },
+
+  fecharNewWindow() {
+    cy.go('back')
+    cy.url().should('include', 'browser-windows')
+  },
+
+  fecharPopup() {
+    this.elements.closeBtn().click({force:true})
+  }
+}
